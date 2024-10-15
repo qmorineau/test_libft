3c7e54f3432dd141d32de7f76693324924b88c2f
Quentin Morineau <142665919+qmorineau@users.noreply.github.com>
2024-10-15 11:02:26 +0200

    test

--- test/mandatory/test_strdup.c
+++ test/mandatory/test_strdup.c
@@ -1,8 +1,29 @@
 #include "unit_test.h"
 
+static int assert_strdup(char *string, char *expected)
+{
+	char *s;
+
+	s = ft_strdup(string);
+	if (strcmp(s, expected) == 0)
+	{
+		PRINT_OK();
+		return (0);
+	}
+	else
+	{
+		PRINT_FAIL();
+		return (1);
+	}
+}
+
 int	test_strdup (void)
 {
 	printf("ft_strdup :");
+	assert_strdup("abcefghijklmnopqrstuvwxyz012345789", "abcefghijklmnopqrstuvwxyz012345789");
+	assert_strdup("", "");
+	assert_strdup("\\0\0", "\\0\0");
+	assert_strdup("\t\t\t\t\t\t\t\v\v\n\n", "\t\t\t\t\t\t\t\v\v\n\n");
 	NEWL();
 	return (1);
 }
--- test/mandatory/test_strmapi.c
+++ test/mandatory/test_strmapi.c
@@ -1,5 +1,30 @@
 #include "unit_test.h"
 
+static int assert_strmapi(char *string, char *expected, char (*f)(unsigned int, char))
+{
+	char *s;
+
+	s = ft_strmapi(string, f);
+	if (strcmp(s, expected) == 0)
+	{
+		PRINT_OK();
+		return (0);
+	}
+	else
+	{
+		PRINT_FAIL();
+		return (1);
+	}
+}
+
+int	test_strmapi (void)
+{
+	printf("ft_strmapi :");
+	assert_strmapi("abcdefgh", "ABCDEFGH", toupper);
+	NEWL();
+	return (1);
+}
+
 int	test_strmapi (void)
 {
 	printf("ft_strmapi :");
