5d00f043cfe3f0dd33be5b81fa32afb8c47ac56d
Quentin Morineau <142665919+qmorineau@users.noreply.github.com>
2024-10-16 13:38:20 +0200

    more test

--- test/mandatory/test_memchr.c
+++ test/mandatory/test_memchr.c
@@ -1,8 +1,47 @@
 #include "unit_test.h"
 
+int	assert_memchr(int search, size_t len, int index_expected)
+{
+	int	tab[10] = {2147483647, -1, 465, 42, -6542313, 0, 119, 19, 1, 123456789};
+
+	if (memchr(tab, search, len) == &tab[index_expected])
+	{
+		PRINT_OK();
+		return (0);
+	}
+	else
+	{
+		PRINT_FAIL();
+		return (1);
+	}
+}
+
+int	assert_memchr_null(int search, size_t len)
+{
+	int	tab[10] = {2147483647, -1, 465, 42, -6542313, 0, 119, 19, 1, 123456789};
+
+	if (ft_memchr(tab, search, len) == NULL)
+	{
+		PRINT_OK();
+		return (0);
+	}
+	else
+	{
+		PRINT_FAIL();
+		return (1);
+	}
+}
+
 int	test_memchr (void)
 {
 	printf("ft_memchr :");
+	assert_memchr(19, sizeof(int) * 9, 7);
+	assert_memchr(19, sizeof(int) * 8, 7);
+	assert_memchr_null(19, sizeof(int) * 7);
+	assert_memchr(2147483647, sizeof(int) * 1, 0);
+	assert_memchr(465, sizeof(int) * 8, 2);
+	assert_memchr_null(19, sizeof(int) * 0);
+	assert_memchr_null(123, sizeof(int) * 10);
 	NEWL();
 	return (1);
 }
--- test/mandatory/test_memcmp.c
+++ test/mandatory/test_memcmp.c
@@ -1,8 +1,30 @@
 #include "unit_test.h"
 
+int assert_memcmp(char *s1, char *s2, size_t len)
+{
+	if (memcmp(s1, s2, len) == ft_memcmp(s1, s2, len))
+	{
+		PRINT_OK();
+		return (1);
+	}
+	else
+	{
+		PRINT_FAIL();
+		return (0);
+	}
+}
+
 int	test_memcmp (void)
 {
 	printf("ft_memcmp :");
+	assert_memcmp("abcdefghijklmnopqrstuvwxy", "abcdefghijklmnopqrstuvwxy", 26);
+	assert_memcmp("abcdefghijklmnopqrstuvwxy", "abcdefghijklmnopqrstuvwx", 26);
+	assert_memcmp("abcdefghijklmnopqrstuvwxy", "abcdefghijklnopqrstuvwxy", 26);
+	assert_memcmp("abcdefghijklmnopqrstuvwxy", "", 10);
+	assert_memcmp("", "abcdefghijklmnopqrstuvwxy", 10);
+	assert_memcmp("", "", 10);
+	assert_memcmp("abcdefghijklmnopqrstuvwxy", "abcdefghijklmnopqrstuvwxy", 0);
+	assert_memcmp("abcdefghijklmnopqrstuvwxy", "abcdefghijklmnopqrstuvwxy", 15);
 	NEWL();
 	return (1);
 }
